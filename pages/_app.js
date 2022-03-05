a048f53eb2bcbcf8d01135c6f6e68ae27f594f79
shyding <shyding@tencent.com>
2022-03-06 01:43:21 +0800

    fix: 使用eslint代码提示

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
-import "../styles/globals.css";
-import "../styles/normalize.css";
-import "antd/dist/antd.css";
-import Header from "../common/header/header";
-import store from "../store/index";
-import { Provider } from "react-redux";
+import '../styles/globals.css'
+import '../styles/normalize.css'
+import 'antd/dist/antd.css'
+import Header from '../common/header/header'
+import store from '../store/index'
+import { Provider } from 'react-redux'
 
 export function Layout({ children }) {
   return (
@@ -11,14 +11,14 @@ export function Layout({ children }) {
       <Header />
       <main>{children}</main>
     </Provider>
-  );
+  )
 }
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Component {...pageProps} />
     </Layout>
-  );
+  )
 }
 
-export default MyApp;
+export default MyApp
